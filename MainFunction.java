129695b10a0de58cdbc1892d6fec2eabb9dfee8f
starlin1995 <653170426@qq.com>
2016-12-23 21:48:34 +0800

    12.23
    mainfunction

--- MainFunction.java
+++ MainFunction.java
@@ -8,16 +8,12 @@ public class MainFunction implements ActionListener {
 
 	Frame frmX = new Frame("医院管理系统");
 
-	// 管理系统选择界面
 	Label k1 = new Label("欢迎使用医院管理系统");
 	Button patientInfo = new Button("病人资料管理");
-	Button price = new Button("相关价格管理");
-	Button chufang = new Button("医生处方管理");
-	Button emp = new Button("医院员工管理");
 
 
 	public MainFunction() {
-		// 选择界面配置
+
 		k1.setBounds(110, 50, 150, 30);
 		patientInfo.setBounds(70, 100, 90, 30);
 		price.setBounds(210, 100, 90, 30);
@@ -38,13 +34,29 @@ public class MainFunction implements ActionListener {
 		frmX.setBackground(Color.ORANGE);
 		frmX.setBounds(500, 200, 350, 270);
 		frmX.setVisible(true);
-
-		// 实现窗口关闭按扭的事件
+		
 		frmX.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
 		});
-		frmX.setResizable(false);// 固定窗口的大小
+		frmX.setResizable(false);
 
 	}
+
+	public void actionPerformed(ActionEvent e) {
+		Object soruce = e.getSource();
+		if (soruce == patientInfo) {
+			new PatientFrame();
+		}
+		if (soruce == price) {
+			new MedicineFrame();
+		}
+		if (soruce == chufang) {
+			new ChufangFrame();
+		}
+		if (soruce == emp) {
+			new EmpFrame();
+		}
+	}
+}
